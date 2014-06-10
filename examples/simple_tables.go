810ae74fa723b946847301936c05e294f3201e87
John-Alan Simmons <simmons.johnalan@gmail.com>
2014-06-10 19:30:27 -0400

    Implemented dynamic rows feature

--- examples/simple_tables.go
+++ examples/simple_tables.go
@@ -37,4 +37,33 @@ func main() {
 	t.AddRow([]string{"defg", "EFGHI", "678"})
 	t.AddRow([]string{"hijkl", "JKL", "9000"})
 	fmt.Println(t.Render())
+
+	fmt.Println("\nSimple table w/ seperators, custom padding, and dynamic rows:")
+
+	t = termtable.NewTable(nil, &termtable.TableOptions{
+		Padding:      3,
+		UseSeparator: true,
+	})
+	t.SetHeader([]string{"LOWERCASE", "UPPERCASE", "NUMBERS"})
+	t.AddRow([]string{"abc", "ABCD", "12345"})
+	t.AddRow([]string{"defg", "EFGHI", "678"})
+	t.AddRow([]string{"hijkl", "JKL", "9000"})
+	fmt.Println(t.Render())
+	t.AddRow([]string{"mnop", "MNO", "479108"})
+	fmt.Println(t.RenderDynamic())
+
+	fmt.Println("\nSimple table w/ seperators, custom padding, dynamic rows, and column wrapping:")
+
+	t = termtable.NewTable(nil, &termtable.TableOptions{
+		Padding:      3,
+		UseSeparator: true,
+	})
+	t.SetHeader([]string{"LOWERCASE", "UPPERCASE", "NUMBERS"})
+	t.AddRow([]string{"abc", "ABCD", "12345"})
+	t.AddRow([]string{"defg", "EFGHI", "678"})
+	t.AddRow([]string{"hijkl", "JKL", "9000"})
+	fmt.Println(t.Render())
+	t.AddRow([]string{"mnop", "MASDADADNO", "47910987978"})
+	t.AddRow([]string{"qrstuvwxyz", "QRS", "54234"})
+	fmt.Println(t.RenderDynamic())
 }
